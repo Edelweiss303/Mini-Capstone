fb74fa1cee109819a9c1e91f11b00aa39c726fc5
Rishi Barnwal <barnwal.rishi@gmail.com>
2020-01-21 12:47:09 -0500

    Button code

---
+++ Assets/Scenes/Scripts/OnClickFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickFunctions : MonoBehaviour
+{
+    public Button[] buttons;
+    public Text displayText;
+
+    private void Start()
+    {
+        foreach(Button b in buttons)
+        {
+            b.onClick.AddListener(delegate { DisplayButtonText(b.GetComponentInChildren<Text>().text); });
+        }
+    }
+
+    public void DisplayButtonText(string s)
+    {
+        displayText.text = s;
+    }
+    
+}
