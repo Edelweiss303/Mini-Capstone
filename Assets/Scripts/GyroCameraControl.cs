13a25c6616d5aa7dc7c9e2ba31f660e124ebc40d
Rishi Barnwal <barnwal.rishi@gmail.com>
2020-01-25 18:52:04 -0500

    Experimented with Gyro Controls in Unity Camera is now controllable with gyroscope

---
+++ Assets/Scripts/GyroCameraControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroCameraControl : MonoBehaviour
+{
+    private Gyroscope gyro;
+
+    void Awake()
+    {
+        gyro = Input.gyro;
+        gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GyroMoveCamera();
+    }
+
+    void GyroMoveCamera()
+    {
+        this.transform.rotation = GyroToUnity(gyro.attitude);
+    }
+
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+}
